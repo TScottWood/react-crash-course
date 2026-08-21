79b3578d9ad8cb860d50e4d71dd9311a6ca25585
Scott <Scott.Wood@TScottWood.com>
2026-08-20 23:25:56 -0500

    4th

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,20 @@ function App() {
     <div>
       <Title />
       <div className="todo__wrapper">
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo
+          title="Finish Frontend Simplified"
+          paragraph="Code along with Frontend Simplified step by step"
+        />
+        <Todo
+          title="Finish interview Section"
+          paragraph="Finish every interview question in the next 6 weeks."
+        />
+        <Todo
+          title="Land a $100k Job"
+          paragraph="Apply to 100 jobs"
+        />
       </div>
-      <Modal />
+      <Modal text="Are you sure you want to delete?" />
     </div>
   );
 }
