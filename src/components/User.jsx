84895a5cff51f9cbeffcdc7554e7c9c0024b06f5
Scott <Scott.Wood@TScottWood.com>
2026-09-22 22:30:53 -0500

    11th

---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+function user ({ id, name, email, username }) {
+    return (
+        <div style={{ border: `2px solid black` }}>
+            <div>{id}</div>
+            <div>{name.toUpperCase()}</div>
+            <div>{email}</div>
+            <div>{username}</div>
+        </div>
+    );    
+}
+
+export default user;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import User from "../components/User.jsx"
 
 function Home() {
     const [users, setUsers] = useState ([])
 
     async function fetchUsers() {
         const{ data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-        setUsers(data)    
+        setUsers(data)
+        console.log(data)    
         }
 
     useEffect(() => {
-        fetchUsers();
-    }, [])
+        setTimeout(() => {    
+            fetchUsers();
+        }, 500);    
+    }, []);
+    const pixels = "3px";
 
-    return <h1>{users.length > 0 && users[0].name}</h1>;
+    return (
+        <div>
+            {users.map((user) => (
+                <User
+                    key={user.id}
+                    id={user.id} 
+                    name={user.name} 
+                    email={user.email} 
+                    username={user.username} 
+                />
+            ))}
+        </div>
+    );
 }
 
 export default Home
